33d68cb172a8a3c1fa21953e3f13fbd8ac0095e8
Tony Alvarez <t.alvarezpascual@gmail.com>
2016-06-06 16:26:50 +0200

    Modificar plantillas de la sección de autenticación

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,16 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+            <div class="panel panel-primary mt-150">
+                <div class="panel-heading">Formulario de registro</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Name</label>
+                            <label class="col-md-5 control-label">Nombre</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input type="text" class="form-control" name="name" value="{{ old('name') }}">
 
                                 @if ($errors->has('name'))
@@ -25,9 +25,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <label class="col-md-5 control-label">Email</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -39,9 +39,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label class="col-md-5 control-label">Contraseña</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
@@ -53,9 +53,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
+                            <label class="col-md-5 control-label">Confirmar contraseña</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input type="password" class="form-control" name="password_confirmation">
 
                                 @if ($errors->has('password_confirmation'))
@@ -67,9 +67,9 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-5">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
+                                    <i class="fa fa-btn fa-user"></i>Registrarme
                                 </button>
                             </div>
                         </div>
